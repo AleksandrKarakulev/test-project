eb50f9c90cf4cc50dfbef18fe6791a7920bcc356
Aleksandr Karakulev <aleksandr.karakulev@it.uu.se>
2023-04-24 14:09:27 +0200

    Add fency output function

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ def main():
     print(f"Hello {username}!")
 
 
+def fency_output(line):
+    print("=============")
+    print(line)
+    print("=============")
+
+
 if __name__ == "__main__":
     main()
