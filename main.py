99ccc97071e27868bd58bff5be700b3b477c2d6c
Aleksandr Karakulev <aleksandr.karakulev@it.uu.se>
2023-04-24 14:18:52 +0200

    Add function to make a valid name from input

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def main():
     username = input()
+    if not check_is_name(username):
+        username = fix_name(username)
     greeting = f"Hello {username}"
     fency_output(greeting)
 
@@ -13,6 +15,11 @@ def fency_output(line):
 def check_is_name(username):
     first_letter = username[0]
     return first_letter.isupper()
+
+
+def fix_name(username):
+    username = username[0].upper() + username[1:]
+    return username
 
 
 if __name__ == "__main__":
